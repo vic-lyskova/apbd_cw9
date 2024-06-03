a56e731a74d97e0be47b7f48ba8de33646382474
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-06-03 23:58:37 +0200

    Endpoint for http delete

--- FirstEntityFrameworkWebApi/FirstEntityFrameworkWebApi/Controllers/TripsController.cs
+++ FirstEntityFrameworkWebApi/FirstEntityFrameworkWebApi/Controllers/TripsController.cs
@@ -1,4 +1,6 @@
 ﻿using FirstEntityFrameworkWebApi.Context;
+using FirstEntityFrameworkWebApi.DTOs;
+using FirstEntityFrameworkWebApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -55,6 +57,42 @@ public class TripsController : ControllerBase
         }
 
         _context.Clients.Remove(_context.Clients.First(cl => cl.IdClient == idClient));
+        await _context.SaveChangesAsync();
+        return Ok();
+    }
+
+    [HttpPost("/api/[controller]/{idTrip:int}/clients")]
+    public async Task<IActionResult> AddClientToTrip(int idTrip, AddClientToTripDTO addClientToTripDto)
+    {
+        var client = _context.Clients.First(cl => cl.Pesel.Equals(addClientToTripDto.Pesel));
+        if (_context.Clients.Any(cl => cl.Pesel.Equals(addClientToTripDto.Pesel)))
+        {
+            return BadRequest("Client already exists");
+        }
+
+        if (_context.ClientTrips.Any(clt => clt.IdTrip == idTrip))
+        {
+            return BadRequest("Client already booked the trip");
+        }
+
+        if (!_context.Trips.Any(t => t.IdTrip == addClientToTripDto.IdTrip))
+        {
+            return NotFound("Trip doesn't exist");
+        }
+
+        if (_context.Trips.First(t => t.IdTrip == addClientToTripDto.IdTrip).DateFrom < DateTime.Today)
+        {
+            return NotFound("The trip was in  the past");
+        }
+
+        _context.ClientTrips.Add(new ClientTrip
+        {
+            IdClient = client.IdClient,
+            IdTrip = idTrip,
+            RegisteredAt = DateTime.Now,
+            PaymentDate = DateTime.Parse(addClientToTripDto.PaymentDate)
+        });
+
         await _context.SaveChangesAsync();
         return Ok();
     }
---
+++ FirstEntityFrameworkWebApi/FirstEntityFrameworkWebApi/DTOs/AddClientToTripDTO.cs
@@ -0,0 +1,13 @@
+namespace FirstEntityFrameworkWebApi.DTOs;
+
+public class AddClientToTripDTO
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Telephone { get; set; }
+    public string Pesel { get; set; }
+    public int IdTrip { get; set; }
+    public string TripName { get; set; }
+    public string? PaymentDate { get; set; }
+}
